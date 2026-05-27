2b484596f01cbc117aab578829a9b3f362872be9
Spencer Garnets <210263209+SpencerGarnets@users.noreply.github.com>
2026-05-27 13:40:45 -0400

    exclude target/ build artifacts from version control

--- target/debug/build/serde_core-2a188d665a976ec2/build_script_build-2a188d665a976ec2.d
+++
@@ -1,5 +0,0 @@
-/private/tmp/citus-local-staging/target/debug/build/serde_core-2a188d665a976ec2/build_script_build-2a188d665a976ec2.d: /Users/spencer/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_core-1.0.228/build.rs
-
-/private/tmp/citus-local-staging/target/debug/build/serde_core-2a188d665a976ec2/build_script_build-2a188d665a976ec2: /Users/spencer/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_core-1.0.228/build.rs
-
-/Users/spencer/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_core-1.0.228/build.rs:
